c681eb11d62f552406e64eaf355effe1678d525d
dariu <dariu@myhost.mshome.net>
2021-04-22 11:10:22 +0200

    first commit

---
+++ timsHowManyCaracters/src/timsHowManyCaracters/Main.java
@@ -0,0 +1,46 @@
+package timsHowManyCaracters;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Map<Character, Integer> map_of_how_many_chars = new LinkedHashMap<Character, Integer>();
+//		System.out.println(map_of_how_many_chars);
+		
+//		String my_string = "Darek ma 3 koty.";
+//		System.out.println(my_string);
+		
+		@SuppressWarnings("resource")
+		Scanner my_scanner = new Scanner(System.in);
+		String my_string = my_scanner.nextLine();
+
+		if (my_string.isEmpty()) {
+			System.out.println("your string is empty. that's the end!");
+			return;
+		}
+		
+		for(int counter = 0 ; counter < my_string.length() ; ++counter)
+		{
+//			System.out.println(my_string.charAt(counter));
+			
+			char char_from_string = my_string.charAt(counter);
+			if(map_of_how_many_chars.containsKey(char_from_string)) {
+				int how_many_chars = (int) map_of_how_many_chars.get(char_from_string);
+				map_of_how_many_chars.put(char_from_string, ++how_many_chars);	
+			}
+			else
+			{
+				map_of_how_many_chars.put(char_from_string, 1);				
+			}
+			
+		}
+		
+		System.out.println(map_of_how_many_chars);
+		
+	}
+
+}
